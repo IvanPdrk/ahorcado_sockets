d71a0bdcf025edb1dcc236a0c9da64a1d954c42d
Ivan Uribe <ivan.urb.mndz@gmail.com>
2019-10-01 10:17:46 -0400

    Ready la sincronia entre jugadores con barrera y lock

--- echo-client.py
+++ echo-client.py
@@ -7,18 +7,26 @@ buffer_size = 1024
 
 
 def conexion(): #Funcion que recibe Ip y Port del usuario
-    addr=input('Enter ip address: ')
-    port=input('Enter port: ')
-    #addr='127.0.0.1'
-    #port=1234
-    return  addr,port
+    #addr=input('Enter ip address: ')
+    #port=input('Enter port: ')
+    addr='10.100.74.228'
+    port=1234
+    return addr,port
+
+
+def difficult(TCPClientSocket,data):#Funcion en sincronia con welcome from server
+    difficult = input("dif"+data)
+    difficult = difficult.upper()
+    data = str.encode(difficult)
+    TCPClientSocket.sendall(data)  # Envia dificultad
+
 
 
 def send_recive_data(TCPClientSocket):
     while True: #COMUNICACION JUEGO
-        data = TCPClientSocket.recv(buffer_size)  # recive msg from server respuesta
-        data = data.decode(encoding="utf-8")      # o bandera de perdeor o ganador
-        if data == 'W':
+        data = TCPClientSocket.recv(buffer_size)  # recive msg from server correct or incorrect
+        data = data.decode(encoding="utf-8")      # o bandera de perdedor o ganador
+        if data == 'W':                           #o Peticion de otra letra
             print('YOU WIN')
             return False
         elif data == 'L':
@@ -46,24 +54,22 @@ def send_recive_data(TCPClientSocket):
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
-    addr,port=conexion() #
+    addr,port=conexion()
     TCPClientSocket.connect(( addr, int(port) ))
-
-    data = TCPClientSocket.recv(buffer_size)  # Recibe mensaje welcome(difficultad) o P2
+    data = TCPClientSocket.recv(buffer_size)  #Recibe mensaje welcome players o P2
     data = data.decode(encoding="utf-8")
-
     if data == 'P2':
-        time.sleep(0.01)
-        data = TCPClientSocket.recv(buffer_size)
+        data=b'P2 recived'
+        TCPClientSocket.sendall(data)
+        time.sleep(0.1)
+        """data = TCPClientSocket.recv(buffer_size)
         data = data.decode(encoding="utf-8")
-        print('tablero:' + data)
-        send_recive_data(TCPClientSocket) #Llamada de funcion que inicia la comunicacion del juego
+        print('tablero:' + data*2)
+        time.sleep(0.5)"""
 
+        send_recive_data(TCPClientSocket) #Llamada de funcion que inicia la comunicacion del juego
     else:                           #Solo entra Player 1
-        difficult = input(data)
-        difficult = difficult.upper()
-        data = str.encode(difficult)
-        TCPClientSocket.sendall(data) #Envia dificultad
+        difficult(TCPClientSocket,data) #Envio y recepcion de mensajes entre esta funcion y welcome de servidor
         send_recive_data(TCPClientSocket)
 
 
--- threadServer.py
+++ threadServer.py
@@ -5,7 +5,11 @@ import sys
 import threading
 import random
 import time
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
 
 
 easy = ['acosadores', 'justiciera', 'educativas', 'eclipsante',
@@ -15,6 +19,7 @@ hard = ['constantinopolitanos', 'carboximetilcelulosa', 'contrarevolucionaria',
            'descongestionamiento', 'electroluminiscentes', 'electronegatividades',
            'hiperlipoproteinemia', 'microemprendimientos', 'semipresidencialista',
            'superplastificadores']
+
 
 word = ''
 tries = 0
@@ -30,70 +35,81 @@ def msg_all(mensaje):
         except:
             listaConexiones.remove(c)
 
-def welcome(conn):
-    data = ' W E L C O M E  T O  A H O R C A D O \nDifficulty: \n [E]asy \n [H]ard \n'
-    data = str.encode(data)
+def difficult(conn):
+    data = bytes("W E L C O M E  T O  A H O R C A D O\nDifficulty: \n [E]asy \n [H]ard \n", 'ascii')
     conn.sendall(data)  # Send welcome message
     data = conn.recv(1024)  # Recive diffuculty answer
     data = data.decode(encoding="utf-8")
     return data
 
-def ahorcado(conn,word):
+
+def release(barrier):
+    logging.debug('Waitting Barrier')
+    current=barrier.wait()
+    logging.debug('Barrier release')
+
+
+def ahorcado(conn,word,barrier,lock):
     global index,indexes,tries,hidden_word,tablero
+    release(barrier)
+    time.sleep(0.5)
+    #creo que aqui va la cola para los turnos
     while True:
-        worst = 0
-        data = b'\nIngresa letra\n'
-        conn.sendall(data)
-        print('Usuario ingresando letra...')
-        data = conn.recv(1024)  # Recive new letter
-        new_letter = data.decode(encoding="utf-8")  # New letter decode
-        for letter in word:
-            if letter == new_letter:
-                index += new_letter  # Index if is in hidden word
-                indexes += new_letter  # Index in string of tries
-        if not index:  # Isnt the new letter
-            tries += 1
-            response = bytes("worst :(\n", 'ascii')  # Send answer
-            conn.sendall(response)
-        else:
-            response = bytes("great (:\n", 'ascii')  # send answer
-            conn.sendall(response)
-        data = conn.recv(1024)  # respuesta confirmada
-        for letter in word:
-            if letter in indexes:
-                hidden_word += letter + ' '
+        with lock:
+            worst = 0
+            data = b'\nIngresa letra\n'
+            conn.sendall(data)
+            logging.debug('Usuario ingresando letra...')
+            data = conn.recv(1024)  # Recive new letter
+            new_letter = data.decode(encoding="utf-8")  # New letter decode
+            for letter in word:
+                if letter == new_letter:
+                    index += new_letter  # Index if is in hidden word
+                    indexes += new_letter  # Index in string of tries
+            if not index:  # Isnt the new letter
+                tries += 1
+                response = bytes("worst :(\n", 'ascii')  # Send answer
+                conn.sendall(response)
             else:
-                hidden_word += '_ '
-                worst += 1
-        tablero = hidden_word
-        table = str.encode(hidden_word)  # encode hidden_word
-        conn.sendall(table)  # Send table
-        print(hidden_word)  # There is the current hidden_word
-        data = conn.recv(1024)  # tablero confirmado
-        print(worst)
-        if worst == 0:  # YOU WIN
-            response = bytes("W", 'ascii')
-            time.sleep(0.01)
-            #conn.sendall(response)
-            msg_all(response)
-            return False
-        if tries == 6:
-            response = bytes("L", 'ascii')
-            time.sleep(0.01)
-            #conn.sendall(response)
-            msg_all(response)
-            return False
-        hidden_word = ''  # reset to clean string
-        index = ''  # Reset to clean index comodin
+                response = bytes("great (:\n", 'ascii')  # send answer
+                conn.sendall(response)
+            data = conn.recv(1024)  # respuesta confirmada
+            for letter in word:
+                if letter in indexes:
+                    hidden_word += letter + ' '
+                else:
+                    hidden_word += '_ '
+                    worst += 1
+            tablero = hidden_word
+            table = str.encode(hidden_word)  # encode hidden_word
+            conn.sendall(table)  # Send table
+            print(hidden_word)  # There is the current hidden_word
+            data = conn.recv(1024)  # tablero confirmado
+            print(worst)
+            if worst == 0:  # YOU WIN
+                response = bytes("W", 'ascii')
+                time.sleep(0.01)
+                #conn.sendall(response)
+                msg_all(response)
+                return False
+            if tries == 6:
+                response = bytes("L", 'ascii')
+                time.sleep(0.01)
+                #conn.sendall(response)
+                msg_all(response)
+                return False
+            hidden_word = ''  # reset to clean string
+            index = ''  # Reset to clean index comodin
+        logging.debug('candado liberado')
 
-
-def servirPorSiempre(socketTcp, listaconexiones):
+def servirPorSiempre(socketTcp, listaconexiones, barrier, lock):
+    global num_players
     try:
         while True:
             client_conn, client_addr = socketTcp.accept()   #Conexion done
             print("Conectado a", client_addr)
             listaconexiones.append(client_conn)
-            thread_read = threading.Thread(target=recibir_datos, args=[client_conn, client_addr])
+            thread_read = threading.Thread(target=recibir_datos, args=[client_conn, client_addr, lock, barrier])
             thread_read.start()
             #thread_read.join()
             gestion_conexiones(listaConexiones)
@@ -110,8 +126,8 @@ def gestion_conexiones(listaconexiones):
     print(listaconexiones)
 
 
-def recibir_datos(conn, addr):
-    global tablero,word
+def recibir_datos(conn, addr, barrier, lock):
+    global tablero, word, num_players
     try:
         cur_thread = threading.current_thread()
         print("Recibiendo datos del cliente {} en el {}".format(addr, cur_thread.name))
@@ -119,23 +135,23 @@ def recibir_datos(conn, addr):
             if len(listaConexiones) > 1:
                 data=b'P2'
                 conn.sendall(data) #Send P2
-                table=str.encode(tablero)
-                time.sleep(0.01)
+                data=conn.recv(1024) #P2 confirmed
+                """table=str.encode(tablero)
+                time.sleep(0.1)
                 msg_all(table) #Send table
-                time.sleep(0.01)
-                ahorcado(conn,word)
-
+                time.sleep(0.1)"""
+                ahorcado(conn,word,barrier,lock) #Call ahorcado
             else:
-                data=welcome(conn)
-                if data == 'E':
+                dif = difficult(conn)   #Call Function to messages, define difficult
+                if dif == 'E':
                     x = random.randrange(10)
                     word = easy[x]
-                    ahorcado(conn,word)
+                    ahorcado(conn,word,barrier,lock)
                     break
-                elif data == 'H':
+                elif dif == 'H':
                     x = random.randrange(10)
                     word = hard[x]
-                    ahorcado(conn,word)
+                    ahorcado(conn,word,barrier,lock)
                     break
             if not data:
                 print("Fin")
@@ -159,8 +175,13 @@ serveraddr = (host, int(port))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
     TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    num_players=input('Hoy many players do you want?')
+    num_players=int(num_players)
+    barrier=threading.Barrier(num_players)
+    lock=threading.Lock()
     TCPServerSocket.bind(serveraddr)
     TCPServerSocket.listen(int(numConn))
     print("El servidor TCP está disponible y en espera de solicitudes")
 
-    servirPorSiempre(TCPServerSocket, listaConexiones)
+
+    servirPorSiempre(TCPServerSocket, listaConexiones, lock, barrier)
